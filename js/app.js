bca02d6e33eaad785eba44412e66fc24818a2ab1
alaaalmasri <alaaalmasri272@gmail.com>
2020-03-03 15:23:07 +0200

    added local storage

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 'use script';
 var totalview = 0;
-var totalclicks=0;
-votes=[];
-views=[];
-var usedimages=[];
-var player=prompt("do you want to use player in ur web page");
-player.toLowerCase();
-if(player==="yes")
-{
-  document.getElementById("player").style.display="block";
+var totalclicks = 0;
+votes = [];
+views = [];
+var usedimages = [];
+var player = prompt("do you want to use player in ur web page").toLowerCase();
+if (player === "yes") {
+  document.getElementById("player").style.display = "block";
 }
-else
-{
-  document.getElementById("player").style.display="none";
+else {
+  document.getElementById("player").style.display = "none";
 
 }
 var products =
@@ -39,35 +36,56 @@ var products =
     "water-can.jpg",
     "wine-glass.jpg"
   ]
+  products.all = [];
+  for (var i = 0; i < products.length; i++) {
+    new Productitem(products[i]);
+  }
+function savetrackvalue() {
+ localStorage.setItem('total', totalview);
 
+}
+function trackvalue() {
+  localStorage.getItem('total');
 
+}
+function updateproducts() //store object to jason
+{
+  var product = JSON.stringify(products.all);
+  localStorage.setItem('products', product);
+  console.log(product);
+}
+function getproducts() {
+  var product = localStorage.getItem('products');
+  if(product) {
+    products.all = JSON.parse(product);
+    console.log(products.all);
 
+  }
+}
+getproducts();
 console.log(products);
 var leftproduct = document.getElementById("leftproduct");
 var centerproduct = document.getElementById("centerproduct");
 var rightproduct = document.getElementById("rightproduct");
 var imagesection = document.querySelector(".products-container");
 leftproduct.src = `assets/${products[0]}`;
-leftproduct.alt = products[0].split(".",1);
-leftproduct.title = products[0].split(".",1);
+leftproduct.alt = products[0].split(".", 1);
+leftproduct.title = products[0].split(".", 1);
 centerproduct.src = `assets/${products[1]}`;
-centerproduct.alt = products[1].split(".",1);
-centerproduct.title = products[1].split(".",1);
+centerproduct.alt = products[1].split(".", 1);
+centerproduct.title = products[1].split(".", 1);
 rightproduct.setAttribute('src', `assets/${products[2]}`);
 rightproduct.setAttribute('alt', products[2]);
 rightproduct.setAttribute('title', products[2]);
 function Productitem(ProductName, imagePath, clicks, views) {
   this.ProductName = ProductName;
   this.imagePath = `assets/${this.ProductName}`;
-  this.views =0;
-  this.clicks =0;
+  this.views = 0;
+  this.clicks = 0;
   this.numberofdisplay = 0;
   products.all.push(this);
 }
-products.all = [];
-for (var i = 0; i < products.length; i++) {
-  new Productitem(products[i]);
-}
+
 var leftp, centerp, rightp;
 function render() {
   totalview++;
@@ -81,11 +99,10 @@ function render() {
     render();
 
   }
-while(usedimages.includes(leftp)||usedimages.includes(centerp)||usedimages.includes(rightp))
-{
-  rend
-}
-  usedimages=[];
+  while (usedimages.includes(leftp) || usedimages.includes(centerp) || usedimages.includes(rightp)) {
+    rend
+  }
+  usedimages = [];
   usedimages.push(leftp.ProductName);
   usedimages.push(centerp.ProductName);
   usedimages.push(rightp.ProductName);
@@ -103,7 +120,8 @@ while(usedimages.includes(leftp)||usedimages.includes(centerp)||usedimages.inclu
 render();
 imagesection.addEventListener("click", handleClickOnProduct);
 function handleClickOnProduct(event) {
-
+  savetrackvalue()
+  trackvalue();
   if (totalview < 25) {
     if (event.target.id !== 'products-container') {
       if (event.target.id === 'leftproduct') {
@@ -133,7 +151,9 @@ function handleClickOnProduct(event) {
 
     imagesection.removeEventListener('click', handleClickOnProduct);
     display();
-    displaychart()
+    displaychart();
+    updateproducts();
+
 
 
   }
@@ -145,9 +165,9 @@ function display() {
   for (var i = 0; i < products.all.length; i++) {
     var liE1 = document.createElement('li');
     liE1.classList.add("list");
-      votes.push(products.all[i].clicks);
-      views.push(products.all[i].views);
-      console.log(votes[i]);
+    votes.push(products.all[i].clicks);
+    views.push(products.all[i].views);
+    console.log(votes[i]);
     console.log(views[i]);
     liE1.textContent = `${products.all[i].ProductName} has ${products.all[i].clicks} clicks and ${products.all[i].views} views`;
     ulE1.appendChild(liE1);
@@ -175,7 +195,7 @@ function displaychart() {
         label: '# of Votes' + `${totalclicks}`,
         data: votes,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',          
+          'rgba(255, 99, 132, 0.2)',
           'rgba(255, 99, 132, 0.2)',
           'rgba(255, 99, 132, 0.2)',
           'rgba(255, 99, 132, 0.2)',
@@ -191,67 +211,67 @@ function displaychart() {
           'rgba(255, 99, 132, 0.2)',
           'rgba(255, 99, 132, 0.2)',
           'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
 
 
 
         ],
         borderColor: [
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',    
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-      
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+
 
         ],
         borderWidth: 1
 
       }, {
-        label: 'total of views'+ `${totalview}`,
+        label: 'total of views' + `${totalview}`,
         data: views,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
 
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',  
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-          'rgba(255, 99, 132, 0.2)',          
-        
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+
 
 
         ],
@@ -277,59 +297,51 @@ function displaychart() {
 //     this.audio = audio;
 //     return this;
 //   };
-  
+
 //   Song.prototype = {
 //       playSound: function() {
 //       var audio = document.createElement('audio');
 //       audio.src = this.audio;
 //       audio.play();
 
-var songs=['Blinding_Lights.mp3','Halsey-Graveyard.mp3'];
-var song=new Audio();
-var currentSong=0;
-window.onload=playsong;
-function playsong()
-{
-  song.src="audio/"+songs[currentSong];
+var songs = ['Blinding_Lights.mp3', 'Halsey-Graveyard.mp3'];
+var song = new Audio();
+var currentSong = 0;
+window.onload = playsong;
+function playsong() {
+  song.src = "audio/" + songs[currentSong];
   song.play();
 }
-function playorstopsong()
-{
-  if(song.paused)
-  {
+function playorstopsong() {
+  if (song.paused) {
     song.play();
-    document.getElementById("plays").setAttribute("name","pause-circle-outline");
+    document.getElementById("plays").setAttribute("name", "pause-circle-outline");
   }
-  else
-  {
+  else {
     song.pause();
-    document.getElementById("plays").setAttribute("name","play-outline");
+    document.getElementById("plays").setAttribute("name", "play-outline");
 
   }
 }
-document.getElementById("play").addEventListener("click",playorstopsong);
-document.getElementById("next").addEventListener("click",next);
-document.getElementById("pre").addEventListener("click",prev);
-document.getElementById("play").addEventListener("dblclick",function(){
+document.getElementById("play").addEventListener("click", playorstopsong);
+document.getElementById("next").addEventListener("click", next);
+document.getElementById("pre").addEventListener("click", prev);
+document.getElementById("play").addEventListener("dblclick", function () {
   song.pause();
 });
 
 
-function next()
-{
+function next() {
   currentSong++;
-  if(currentSong>2)
-  {
-    currentSong=0
+  if (currentSong > 2) {
+    currentSong = 0
   }
   playsong();
 }
-function prev()
-{
+function prev() {
   currentSong--;
-  if(currentSong<0)
-  {
-    currentSong=2;
+  if (currentSong < 0) {
+    currentSong = 2;
   }
   playsong();
 }
