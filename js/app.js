292a24ced20ab7d27876aaa1d057597e004650af
alaaalmasri <alaaalmasri272@gmail.com>
2020-03-03 07:55:31 +0200

    sada

--- js/app.js
+++ js/app.js
@@ -213,3 +213,7 @@ function PlayFunc(target, RepeatCount) {
   target.addEventListener('ended', soundFunc)
   target.play();
 }
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
